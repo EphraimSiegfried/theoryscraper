7c2fd7cebd4dec50aa64f618eafeae9a504276e0
Ephraim Siegfried <e.siegfried@stud.unibas.ch>
2023-06-23 10:52:58 +0200

    generalize extraction, e.g.theorems are also extracted

--- generate_cheatsheet.py
+++ generate_cheatsheet.py
@@ -1,7 +1,7 @@
 
 import json
 
-def generate_html_cheatsheet(definitions, filename):
+def generate_html_cheatsheet(theory, filename):
     with open(filename, 'w') as f:
         f.write('<html><head>\n')
         f.write('<style>\n')
@@ -9,14 +9,20 @@ def generate_html_cheatsheet(definitions, filename):
         f.write('.columns { column-count: 3; }\n')
         f.write('@media print { @page { size: A4; } }\n')
         f.write('</style>\n')
-        f.write('</head><body><div class="columns">\n')
-        for definition in definitions:
-            name, body = definition['name'], definition['content']
-            f.write('<p><b>{}</b>{}</p>\n'.format(name, body))
-        f.write('</div></body></html>\n')
+        f.write('</head><body>\n')
 
-# Load definitions from JSON file
-with open('definitions.json', 'r') as f:
-    definitions = json.load(f)
+        for searchterm, definitions in theory.items():
+            f.write('<h1>{}s</h1>\n'.format(searchterm))
+            f.write('<div class="columns">\n')
+            for definition in definitions:
+                name, body = definition['name'], definition['content']
+                f.write('<p><b>{}</b>{}</p>\n'.format(name, body))
+            f.write('</div>\n')
 
-generate_html_cheatsheet(definitions, 'cheatsheet.html')
+        f.write('</body></html>\n')
+
+# Load theory from JSON file
+with open('theory.json', 'r') as f:
+    theory = json.load(f)
+
+generate_html_cheatsheet(theory, 'cheatsheet.html')
--- scraper.py
+++ scraper.py
@@ -1,57 +1,60 @@
+
 import os
 import json
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
 import re
 
-def get_definitions(page_layout):
-    definitions = []
+
+def get_theory(page_layout, searchterm, delimiters, encountered_content):
+    theory_list = []
     recording = False
-    current_definition = {'name': '', 'content': ''}
-    encountered_names = set()
+    current_theory = {'name': '', 'content': ''}
 
     for element in page_layout:
         if isinstance(element, LTTextContainer):
             for text_line in element:
                 line = text_line.get_text().strip()
-                if line.startswith('Definition'):
+                if line.startswith(searchterm):
                     recording = True
-                    match = re.match(r'Definition\s*\((.*?)\)', line)
+                    match = re.match(r'{}\s*\((.*?)\)'.format(searchterm), line)
                     if match:
                         name = match.group(1)
-                        if name not in encountered_names:
-                            encountered_names.add(name)
-                            current_definition['name'] = name
-                            current_definition['content'] = line[match.end():] + '\n'
-                        else:
-                            recording = False
+                        current_theory['name'] = name
+                        current_theory['content'] = line[match.end():] + '\n'
                 elif recording:
-                    if line == '' or line.startswith('Gabriele') or line.startswith('Example') or line.startswith('Theorem'):
+                    if line == '' or any([line.startswith(delimiter) for delimiter in delimiters]):
                         recording = False
-                        definitions.append(current_definition)
-                        current_definition = {'name': '', 'content': ''}
+                        if current_theory['content'] not in encountered_content:
+                            encountered_content.add(current_theory['content'])
+                            theory_list.append(current_theory)
+                        current_theory = {'name': '', 'content': ''}
                     else:
-                        current_definition['content'] += line + '\n'
+                        current_theory['content'] += line + '\n'
 
     if recording:
-        definitions.append(current_definition)
+        if current_theory['content'] not in encountered_content:
+            theory_list.append(current_theory)
 
-    return definitions
+    return theory_list
 
-def extract_definitions_from_pdf_files(folder_path):
+def extract_theory_from_pdf_files(folder_path, search_terms):
     pdf_files = sorted([f for f in os.listdir(folder_path) if f.endswith('.pdf')])
-    all_definitions = []
+    all_theory = {}
+    encountered_content = set()
 
-    for pdf_file in pdf_files:
-        for page_layout in extract_pages(os.path.join(folder_path, pdf_file)):
-            definitions = get_definitions(page_layout)
-            all_definitions.extend(definitions)
+    for search_term in search_terms:
+        all_theory[search_term] = []
+        for pdf_file in pdf_files:
+            for page_layout in extract_pages(os.path.join(folder_path, pdf_file)):
+                theory = get_theory(page_layout, search_term, search_terms[search_term],encountered_content)
+                all_theory[search_term].extend(theory)
 
-    return all_definitions
+    return all_theory
 
-folder_path = input("Folder with slides" )
-definitions = extract_definitions_from_pdf_files(folder_path)
-
-with open('definitions.json', 'w') as f:
-    json.dump(definitions, f,indent=4)
-
+folder_path = input("Folder with slides: " )
+search_terms = {"Definition":["Gabriele", "Proof", "Theorem"], "Theorem":["Proof", "Example","Gabriele","Question"]}
+theory = extract_theory_from_pdf_files(folder_path, search_terms)
+print(theory)
+with open('theory.json', 'w') as f:
+    json.dump(theory, f,indent=4)
